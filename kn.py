abb0e22d4df6b797d396d7215e5210832d592249
The Aperture Project <apertureproject.me@gmail.com>
2018-11-11 16:45:13 +0100

    Update kn.py

--- kn.py
+++ kn.py
@@ -43,8 +43,6 @@ bot.remove_command('help')
 
 
 class DiscordBotsOrgAPI:
-	"""Handles interactions with the discordbots.org API"""
-
 	def __init__(self, bot):
 		self.bot = bot
 		self.token = 'dbl_token'  #  set this to your DBL token
@@ -52,7 +50,6 @@ class DiscordBotsOrgAPI:
 		self.bot.loop.create_task(self.update_stats())
 	
 	async def update_stats(self):
-	"""This function runs every 30 minutes to automatically update your server count"""
 		while True:
 			try:
 				await self.dblpy.post_server_count()

906a3763f36bf164f7672ec09861b0b2367ccbf8
The Aperture Project <apertureproject.me@gmail.com>
2018-11-17 23:40:07 +0100

    Update kn.py

--- kn.py
+++ kn.py
@@ -162,6 +162,7 @@ async def help_utilities(ctx):
 	c = discord.Embed(description='⚒️ Utilities ⚒️', title='➡️Commands list', color=0x003366, timestamp=datetime.utcnow())
 	c.set_thumbnail(url="https://cdn.discordapp.com/emojis/395627468276367370.png?v=1")
 	c.add_field(name=f'<{prefiximg}>`pp <user>`', value='Get the profile picture of some user')
+	c.add_field(name=f'<{prefiximg}>`wiki <request>`', value='Search WikiPedia for whatever you want !')
 	await ctx.send(embed=c)
 
 @help.command(name="moderator")
